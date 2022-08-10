6c52beefa5f89dc733d0c0e294dd5158fb9de90c
armin shoughi <arminshoughi@gmail.com>
2022-08-10 19:56:08 +0430

    working on models

---
+++ backend/daneshjoei/base/models/__init__.py
@@ -0,0 +1,5 @@
+from .base import *
+from .course import *
+from .professor import *
+from .student import *
+from .term import *
---
+++ backend/daneshjoei/utils/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_created=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(blank=True, null=True)
