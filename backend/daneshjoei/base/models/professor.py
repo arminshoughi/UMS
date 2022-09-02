3a4680b1b766d67cd860b5b7d8239558325d4c23
armin shoughi <arminshoughi@gmail.com>
2022-09-02 14:49:23 +0430

    reconstruct project tree

--- backend/daneshjoei/base/models/professor.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from . import Degree, Person, Course
-
-
-class Professor(models.Model):
-    degree = models.ForeignKey(verbose_name='مقطع', to=Degree, related_name='professor', on_delete=models.PROTECT)
-    person = models.ForeignKey(verbose_name='اطلاعات فردی', to=Person, on_delete=models.CASCADE)
-    courses = models.ManyToManyField(verbose_name='دروس', to=Course)
--- backend/daneshjoei/base/models/student.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-from . import Degree, Person
-
-
-class Student(models.Model):
-    degree = models.ForeignKey(verbose_name='مقطع', to=Degree, related_name='professor', on_delete=models.PROTECT)
-    person = models.ForeignKey(verbose_name='اطلاعات فردی', to=Person, on_delete=models.CASCADE)
