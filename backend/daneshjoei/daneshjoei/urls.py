011eb5ff56d3fb27f788a8acac7c0ef2660354f9
armin shoughi <arminshoughi@gmail.com>
2022-09-09 17:00:07 +0430

    added dockerfile

--- backend/daneshjoei/daneshjoei/urls.py
+++ backend/daneshjoei/daneshjoei/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
