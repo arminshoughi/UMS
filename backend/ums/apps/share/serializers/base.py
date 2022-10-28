2ba3633efc4cd7b63e051fdbd6e3c56d4405e5d3
arminshoughi <arminshoughi@gmail.com>
2022-10-28 20:58:17 +0330

    added price into course model

--- backend/ums/apps/share/serializers/base.py
+++ backend/ums/apps/share/serializers/base.py
@@ -72,5 +72,5 @@ class CourseModelSerializer(DynamicFieldsModelSerializer):
         service = services.CourseService
         fields = [
             'major', 'major_id', 'semester', 'semester_id', 'name', 'details', 'unit', 'master_id', 'master',
-            'documents', 'schedules', 'midterm_exam_date', 'final_exam_date'
+            'documents', 'schedules', 'midterm_exam_date', 'final_exam_date', 'price'
         ]
