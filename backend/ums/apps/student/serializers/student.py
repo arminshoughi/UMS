51adf7f7b6afb336eaf28cd1a892b5e120b15e19
arminshoughi <arminshoughi@gmail.com>
2022-10-31 00:08:39 +0330

    added get and take method for student courses and student semesters

--- backend/ums/apps/student/serializers/student.py
+++ backend/ums/apps/student/serializers/student.py
@@ -1,4 +1,7 @@
-from apps.student.models import StudentModel
+from rest_framework import serializers
+
+from apps.share.models import CourseModel, SemesterModel
+from apps.student.models import StudentModel, StudentSemesterModel, StudentSemesterCourseModel
 from apps.student.services import StudentService
 from utils.serializers import DynamicFieldsModelSerializer
 
@@ -18,7 +21,87 @@ class StudentModelSerializer(StudentModelBaseSerializer):
 
 
 class StudentModelCreateSerializer(StudentModelBaseSerializer):
+    id = serializers.IntegerField(read_only=True)
+
     class Meta(StudentModelBaseSerializer.Meta):
         fields = [
-            'username', 'password', 'first_name', 'last_name', 'collage', 'major', 'national_code', 'sex', 'birthday',
+            'id', 'username', 'password', 'first_name', 'last_name', 'collage', 'major', 'national_code', 'sex',
+            'birthday',
         ]
+
+
+class StudentTakeSemesterSerializer(serializers.Serializer):
+    student_id = serializers.IntegerField()
+    semester_id = serializers.IntegerField()
+
+    def validate_student_id(self, value):
+        student = StudentModel.objects.filter(id=value)
+        if not student.exists():
+            raise serializers.ValidationError('دانشجوی مورد نظر یافت نشد.')
+        return value
+
+    def validate_semester_id(self, value):
+        semester = SemesterModel.objects.filter(id=value)
+        if not semester.exists():
+            raise serializers.ValidationError('ترم مورد نظر یافت نشد.')
+        return value
+
+    def validate(self, attrs):
+        if StudentSemesterModel.objects.filter(
+                student_id=attrs['student_id'], semester_id=attrs['semester_id']
+        ).exists():
+            raise serializers.ValidationError('دانشجو قبلا در این ترم ثبت نام شده است.')
+        return attrs
+
+    def save(self, **kwargs):
+        student = StudentModel.objects.get(id=self.validated_data['student_id'])
+        semester = SemesterModel.objects.get(id=self.validated_data['semester_id'])
+        obj = StudentSemesterModel.objects.create(
+            student=student, semester=semester
+        )
+        obj.save()
+        return obj
+
+    def to_representation(self, instance):
+        rep = super(StudentTakeSemesterSerializer, self).to_representation(instance)
+        if self.instance:
+            rep.update({'id': instance.id})
+        return rep
+
+
+class StudentTakeCourseSerializer(serializers.Serializer):
+    student_semester_id = serializers.IntegerField()
+    course_id = serializers.IntegerField()
+
+    def validate_student_semester_id(self, value):
+        student_semester = StudentSemesterModel.objects.filter(id=value)
+        if not student_semester.exists():
+            raise serializers.ValidationError('دانشجو در ترم مورد نظر ثبت نام نشده است.')
+        return value
+
+    def validate_course_id(self, value):
+        course = CourseModel.objects.filter(id=value)
+        if not course.exists():
+            raise serializers.ValidationError('درس مورد نظر یافت نشد.')
+        return value
+
+    def validate(self, attrs):
+        if StudentSemesterCourseModel.objects.filter(
+                course_id=attrs['course_id'], student_semester_id=attrs['student_semester_id']
+        ).exists():
+            raise serializers.ValidationError('این درس قبلا توسط این دانشجو دراین ترم برداشته شده است.')
+
+        return attrs
+
+    def save(self, **kwargs):
+        student_semester = StudentSemesterModel.objects.get(id=self.validated_data['student_semester_id'])
+        course = CourseModel.objects.get(id=self.validated_data['course_id'])
+        obj = StudentSemesterCourseModel.objects.create(student_semester=student_semester, course=course)
+        obj.save()
+        return obj
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        if self.instance:
+            rep.update({'id': instance.id})
+        return rep
--- backend/ums/apps/student/views/student.py
+++ backend/ums/apps/student/views/student.py
@@ -1,7 +1,11 @@
+from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from apps.student.serializers import StudentModelCreateSerializer
+from apps.student.serializers import StudentModelCreateSerializer, StudentTakeSemesterSerializer, \
+    StudentTakeCourseSerializer, StudentSemesterModel, StudentSemesterCourseModel
 from apps.student.services import StudentService
 
 
@@ -9,3 +13,57 @@ class StudentModelViewSet(ModelViewSet):
     queryset = StudentService.all()
     serializer_class = StudentModelCreateSerializer
     permission_classes = [IsAdminUser]
+
+    @action(
+        detail=False, methods=['GET'], permission_classes=[IsAdminUser], url_name='get_semesters',
+        url_path='get_semesters', serializer_class=StudentTakeSemesterSerializer
+    )
+    def get_semesters(self, request, *args, **kwargs):
+        if not hasattr(self.request.user, 'studentmodel'):
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            data=self.serializer_class(
+                StudentSemesterModel.objects.filter(student=self.request.user.studentmodel), many=True
+            ).data,
+            status=status.HTTP_201_CREATED
+        )
+
+    @action(
+        detail=False, methods=['POST'], permission_classes=[IsAdminUser], url_name='take_semester',
+        url_path='take_semester', serializer_class=StudentTakeSemesterSerializer
+    )
+    def take_semester(self, request, *args, **kwargs):
+        student_term = self.serializer_class(data=self.request.data)
+        student_term.is_valid(raise_exception=True)
+        if self.request.user.studentmodel != StudentService.get(id=student_term.validated_data['student_id']):
+            return Response(data={'خظا': 'دسترسی غیر مجاز'}, status=status.HTTP_400_BAD_REQUEST)
+        student_term.save()
+        return Response(data=student_term.data, status=status.HTTP_201_CREATED)
+
+    @action(
+        detail=False, methods=['GET'], permission_classes=[IsAdminUser], url_name='get_courses',
+        url_path='get_courses', serializer_class=StudentTakeCourseSerializer
+    )
+    def get_courses(self, request, *args, **kwargs):
+        if not hasattr(self.request.user, 'studentmodel'):
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            data=self.serializer_class(
+                StudentSemesterCourseModel.objects.filter(
+                    student_semester__student=self.request.user.studentmodel), many=True
+            ).data,
+            status=status.HTTP_201_CREATED
+        )
+
+    @action(
+        detail=False, methods=['POST'], permission_classes=[IsAdminUser], url_name='take_course',
+        url_path='take_course', serializer_class=StudentTakeCourseSerializer
+    )
+    def take_course(self, request, *args, **kwargs):
+        student_term_course = self.serializer_class(data=self.request.data)
+        student_term_course.is_valid(raise_exception=True)
+        if self.request.user.studentmodel != StudentSemesterModel.objects.get(
+                id=student_term_course.validated_data['student_semester_id']).student:
+            return Response(data={'خظا': 'دسترسی غیر مجاز'}, status=status.HTTP_400_BAD_REQUEST)
+        student_term_course.save()
+        return Response(data=student_term_course.data, status=status.HTTP_201_CREATED)
