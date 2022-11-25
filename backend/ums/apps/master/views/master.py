7e7b837a1040636b84c3fc883c274a4e1d12ce26
arminshoughi <arminshoughi@gmail.com>
2022-11-25 23:44:36 +0330

    master can add student grade (midterm and final exam)

--- backend/ums/apps/master/views/master.py
+++ backend/ums/apps/master/views/master.py
@@ -1,11 +1,24 @@
+from rest_framework import mixins
 from rest_framework.permissions import IsAdminUser
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
-from apps.master.serializers import MasterModelCreateSerializer
+from apps.master.serializers import MasterModelCreateSerializer, StudentGradeListCreateSerializer
 from apps.master.services import MasterService
+from apps.student.services import StudentSemesterCourseModelService
 
 
 class MasterModelViewSet(ModelViewSet):
     queryset = MasterService.all()
     serializer_class = MasterModelCreateSerializer
     permission_classes = [IsAdminUser]
+
+
+class StudentGradeViewSet(
+    mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.ListModelMixin, GenericViewSet
+):
+    queryset = StudentSemesterCourseModelService.all()
+    serializer_class = StudentGradeListCreateSerializer
+    permission_classes = [IsAdminUser]
+
+    def get_queryset(self):
+        return self.queryset.filter(course__master=self.request.user)
